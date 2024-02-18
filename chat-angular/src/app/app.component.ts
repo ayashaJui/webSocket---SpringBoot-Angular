aef6283d5522a983de3d1c868ce1fc2e75bb465b
ayashaJui <jui1801@cseku.ac.bd>
2024-02-18 15:17:50 +0600

    chat app

--- chat-angular/src/app/app.component.ts
+++ chat-angular/src/app/app.component.ts
@@ -1,10 +1,90 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { WebsocketService } from './websocket/websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'chat-angular';
+
+  messageSubscription!: Subscription;
+
+  nameForm!: FormGroup;
+  chatForm!: FormGroup;
+  hideForm: boolean = false;
+  username!: string;
+  messages: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private socketService: WebsocketService
+  ) {
+    this.nameForm = this.fb.group({
+      username: ['', Validators.required],
+    });
+
+    this.chatForm = this.fb.group({
+      content: ['', Validators.required],
+    });
+
+    this.messageSubscription = this.socketService.messageSubject.subscribe(
+      (payload) => {
+        this.handleMessage(payload);
+      }
+    );
+  }
+
+  enterChat(event: any) {
+    event.preventDefault();
+
+    this.username = this.nameForm.get('username')?.value.trim();
+    this.socketService.onConnected(this.username);
+  }
+
+  handleMessage(payload: any) {
+    this.messages.push(JSON.parse(payload.body));
+    this.hideForm = true;
+    console.log(this.messages);
+  }
+
+  getAvatarColor(messageSender: string) {
+    let colors = [
+      '#2196F3',
+      '#32c787',
+      '#00BCD4',
+      '#ff5652',
+      '#ffc107',
+      '#ff85af',
+      '#FF9800',
+      '#39bbb0',
+    ];
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+      hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
+  }
+
+  getMessageClass(message: any) {
+    return message.type === 'JOIN' || message.type === 'LEAVE'
+      ? 'event-message'
+      : 'chat-message';
+  }
+
+  sendMessage() {
+    let message = this.chatForm.get('content')?.value;
+
+    this.socketService.sendMessage(message, this.username);
+
+    this.chatForm.reset();
+  }
+
+  leaveChat() {
+    this.socketService.onDisconnected();
+  }
 }
--- chat-angular/src/app/websocket/websocket.service.ts
+++ chat-angular/src/app/websocket/websocket.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebsocketService {
+  messageSubject: Subject<any> = new Subject<any>();
 
-  constructor() { }
+  webSocketEndPoint: string = 'http://localhost:8080/ws';
+  topic: string = '/topic/public';
+  stompClient: any;
+
+  constructor() {}
+
+  onConnected(name: string) {
+
+    console.log('Initialize WebSocket Connection');
+    let ws = new SockJS(this.webSocketEndPoint);
+    this.stompClient = Stomp.over(ws);
+
+    this.stompClient.connect({}, () => {
+      this.stompClient.subscribe(this.topic, (payload: any) => {
+        this.onMessageRecieved(payload);
+      });
+
+      this.stompClient.send(
+        '/app/chat.addUser',
+        {},
+        JSON.stringify({
+          sender: name,
+          type: 'JOIN',
+        })
+      );
+    });
+  }
+
+  onDisconnected() {
+    this.stompClient.disconnect();
+  }
+
+  onMessageRecieved(payload: any) {
+    this.messageSubject.next(payload);
+  }
+
+  sendMessage(content: string, name: string) {
+    if (this.stompClient && this.stompClient.connected) {
+      this.stompClient.send(
+        '/app/chat.sendMessage',
+        {},
+        JSON.stringify({
+          sender: name,
+          content: content,
+          type: 'CHAT',
+        })
+      );
+    } else {
+      console.error('WebSocket connection is not established.');
+    }
+  }
 }
